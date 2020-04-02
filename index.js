58f816e221459a10b6abe0190fe9acaa2f4d0a54
xxcxy <bert7914@gmail.com>
2020-04-02 19:15:40 +0800

    use slack command

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const { createEventAdapter } = require('@slack/events-api')
-const { WebClient } = require('@slack/web-api')
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
-const slackEvents = createEventAdapter(slackSigningSecret)
+const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000
-const token = process.env.SLACK_TOKEN
 
-const web = new WebClient(token)
-slackEvents.on('message', async (event) => {
-  console.log(JSON.stringify(event))
-  await web.chat.postMessage({
-    text: 'Hello world!',
-    channel: event.channel
-  })
+app.post('/slack/calculate', (req, res) => {
+    console.log(JSON.stringify(req.body))
+    res.send('Hello World!')
 })
 
-// All errors in listeners are caught here. If this weren't caught, the program would terminate.
-slackEvents.on('error', (error) => {
-  console.log(error.name) // TypeError
-});
-
-(async () => {
-  const server = await slackEvents.start(port)
-  console.log(`Listening for events on ${server.address().port}`)
-})()
+app.listen(port, () => console.log(`Example app listening at ${port}`))
