711d2040be3e82a01517d314543b0c9e07c81251
xxcxy <bert7914@gmail.com>
2020-04-02 20:23:05 +0800

    log the param

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.post('/slack/calculate', (req, res) => {
+    console.log(JSON.stringify(req.query))
     console.log(JSON.stringify(req.body))
-    res.send('Hello World!')
+    res.send('')
 })
 
 app.listen(port, () => console.log(`Example app listening at ${port}`))
